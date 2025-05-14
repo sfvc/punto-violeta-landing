f927913bc53fe7278ae53f222418059290bd06ac
Agustin Jalil <agustinjalilleon@gmail.com>
2025-05-14 11:58:57 -0300

    punto violeta landing

--- src/components/pages/queEs.jsx
+++ src/components/pages/queEs.jsx
@@ -5,16 +5,19 @@ const QueEs = () => {
         <Section>
             <div className="que-es">
                 <div className="que-es__content">
-                    <h2 className="que-es__title">Nodo Tecnologico</h2>
+                    <h2 className="que-es__title">Punto Violeta</h2>
                     <em className="que-es__description">
-                        El Nodo Tecnológico de Catamarca conecta innovación y tecnología, impulsando a los jóvenes hacia las habilidades del futuro a través de cursos especializados.
+                        Punto Violeta es una iniciativa de la Secretaría de las Mujeres, Géneros y Diversidad de la Municipalidad de Catamarca Capital.
                     </em>
-                    <small className="que-es__small">Emilio Ramasi - Director del nodo</small>
+                    <small className="que-es__small">
+                        Secretaría de las Mujeres, Géneros y Diversidad - Catamarca Capital<br />
+                        Secretaria: Dra. Gabriela Molina
+                    </small>
                 </div>
                 <div className="que-es__image-container">
                     <img 
                         src="/logo-giro.svg" 
-                        alt="Logo Giro" 
+                        alt="Logo Punto Violeta" 
                         className="que-es__image" 
                     />
                 </div>
